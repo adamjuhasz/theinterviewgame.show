f1a60a5857436c2d971717ab2b020770183eff0d
Adam Juhasz <adam@paytgthr.com>
2022-08-17 08:08:48 -0600

    new questions

--- data/questions.ts
+++ data/questions.ts
@@ -1,9 +1,9 @@
 const questions: Record<string, Record<number, string>> = {
-  "Around the world": {
+  Travelling: {
     100: "If you could choose to live one place in the world where would it be and why?",
     200: "Have you ever taken a vacation by yourself?",
     300: "What is the most interesting place you have been, and why?",
-    400: "Have you ever gotten lost while traveling? Tell us the story.",
+    400: "You're traveling to a deserted island, what three items would you take with you?",
   },
   Hypothetical: {
     100: "If you could live in another historical period, would you? If so, which one?",
@@ -15,13 +15,13 @@ const questions: Record<string, Record<number, string>> = {
     100: "When you were a kid, what did you want to be when you grew up?",
     200: "What is your favorite OG Disney movie?",
     300: "What was your first ever job?",
-    400: "Where were you born?",
+    400: "Who was your best friend growing up?",
   },
   Favorites: {
-    100: "What's one totally irrational fear that you have?",
+    100: "What is one thing you are obsessed with at the moment?",
     200: "What achievement are you most proud of?",
     300: "What shows are you currently watching?",
-    400: "What is your favorite holiday?",
+    400: "If you could share a meal with any 3 famous individuals, living or dead, who would they be?",
   },
   "Pot luck": {
     100: "What topic could you give a 1 hour presentation on with no preparation?",
